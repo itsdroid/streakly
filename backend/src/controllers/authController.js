b55a4f6388d66d554aa6472fea13c34b5f9f3495
itsdroid <shubhampopalghat77@gmail.com>
2026-01-06 23:11:54 +0530

    updated auth

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt.js';
+import jwt from 'jsonwebtoken.js';
+import dotenv from 'dotenv.js';
+import UserModel from '../models/UserModel';
+
+const JWT = process.env.JWT_TOKEN || 'jwt_token_not_found';
+
+
+// -------- user registration ---------
+export default async function registerUser(req, res) {
+    try {
+        const { name, email, password } = req.body;
+        const isAlreadyExists = User.fineOne({ where: { email } });
+
+        if (isAlreadyExists) return res.status(400).json({ message: " user already exists, Please login instead." });
+        
+        const hashedPassword = await bcrypt.hash(password,10);
+        const User = UserModel.create ({
+            name,
+            email,
+            password: hashedPassword
+        });
+        res.status(200).json({ message: "user created successfully ",
+            User: {
+                id: User.id,
+                name: User.name,
+                email: User.email
+            }
+        });
+    }
+    catch(e) {
+        res.status(500).json({ message: "Registration error: " , e});
+    }
+}
+
+
